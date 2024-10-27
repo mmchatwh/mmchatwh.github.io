1eacef3531268234237ef37acb2192dbddfdb81b
mmchatwh <mmchatwh@gmail.com>
2024-10-26 22:46:05 -0400

    stories 1-90 added

--- htmlscript.py
+++ htmlscript.py
@@ -1,10 +1,10 @@
 uris = open("stories.txt", "r")
 images = open("images.txt", "r")
-buttons = open("buttons.txt", "a")
+buttons = open("buttons.txt", "w")
 
 urilist = uris.readlines()
 imagelist = images.readlines()
-for i in range(20):
+for i in range(len(urilist)):
     buttons.write("<!-- story #"+str((i+1))+" --> <input type=\"image\" src=\""+str(imagelist[i]).strip()+"\" class=\"playlist\" data-spotify-id=\"spotify:playlist:"+str(urilist[i]).strip()+"\"/>\n")
 uris.close()
 images.close()
